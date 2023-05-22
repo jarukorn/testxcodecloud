bd900ea1ae02ca2dc2ff740f9213af2d531a7bbd
Jarukorn Thuengjitvilas <jarukorn.t@lmwn.com>
2023-05-22 17:38:42 +0700

    add project

---
+++ xcodecloud/xcodecloud/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  xcodecloud
+//
+//  Created by Jarukorn.T on 22/5/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcodecloud/xcodecloud/xcodecloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodecloudApp.swift
+//  xcodecloud
+//
+//  Created by Jarukorn.T on 22/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct xcodecloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
